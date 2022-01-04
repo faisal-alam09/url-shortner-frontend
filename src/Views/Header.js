be8a0df0961772b2cac0fc18eafa6da2218d8be3
faisal-alam09 <faisal.009alam@gmail.com>
2022-01-05 00:59:02 +0530

    Added header initial code.

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 
 export default function Header() {
     return (
-        <div>
-            <h3>header</h3>
-        </div>
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Url Shortner</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">shortned urls</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                    </Nav>
+                    <span>by faisal-alam</span>
+                </Container>
+            </Navbar>
+        </>
     )
 }
