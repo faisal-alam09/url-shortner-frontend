f80ec8b7846a52e52fde6235f30d25aa1f176341
faisal09alam <faisal.009alam@gmail.com>
2022-01-13 01:37:37 +0530

    minor change

--- src/Views/Result.js
+++ src/Views/Result.js
@@ -8,7 +8,7 @@ export default function Result(props) {
     //console.log(props);
     return (
         <>
-            <div style={{ position: 'relative', padding: '10px', justifyContent: 'center', display: 'inherit' }}>
+            <div style={{ position: 'relative', padding: '10px 0px 0px 0px', justifyContent: 'center', display: 'inherit' }}>
                 {/* <h3>Here is the result...</h3> */}
                 <QRCode value={props.url} />
                 <button className="result-link" onClick={() => { navigator.clipboard.writeText(props.baseUrl + props.url); }}  >
@@ -19,19 +19,6 @@ export default function Result(props) {
                     </svg>
                     {/* {console.log("Props ",props)} */}
                 </button>
-            </div>
-            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="toast-header">
-                    <img class="rounded mr-2" alt="" />
-                        <strong class="mr-auto">Bootstrap</strong>
-                        <small>11 mins ago</small>
-                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                </div>
-                <div class="toast-body">
-                    Hello, world! This is a toast message.
-                </div>
             </div>
         </>
     )
