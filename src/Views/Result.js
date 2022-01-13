7b6bd6a1813f4b32deede8960d031e72b82418ae
faisal09alam <faisal.009alam@gmail.com>
2022-01-13 19:27:32 +0530

    QRCode bug fixed..

--- src/Views/Result.js
+++ src/Views/Result.js
@@ -10,7 +10,7 @@ export default function Result(props) {
         <>
             <div style={{ position: 'relative', padding: '10px 0px 0px 0px', justifyContent: 'center', display: 'inherit' }}>
                 {/* <h3>Here is the result...</h3> */}
-                <QRCode value={props.url} />
+                <QRCode value={props.baseUrl + props.url} size={128} bgColor={"#ffffff"} fgColor={"#000000"} level={"L"} includeMargin={false} renderAs={"svg"} />
                 <button className="result-link" onClick={() => { navigator.clipboard.writeText(props.baseUrl + props.url); }}  >
                     <h5>{props.baseUrl + props.url}</h5>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-clipboard clipboard-img" viewBox="0 0 16 16">
